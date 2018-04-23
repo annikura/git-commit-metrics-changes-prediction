c2f7583028d81c52e258a27c84e6166cfcef7a0f
Ann <annikura@yandex.ru>
2018-04-23 18:34:28 +0300

    Added methods retrieving

--- java_metrics.py
+++ java_metrics.py
@@ -16,13 +16,41 @@ class ClassCountingListener(JavaParserListener):
 class MethodsCountingListener(JavaParserListener):
     def __init__(self):
         self.count = 0
+        self.blocks = {}
+        self.__nested_in = []
+        self.__last_opened = -1
+        self.__in_method_counter = 0
+        self.__current_method = None
+
+    def enterClassDeclaration(self, ctx: JavaParser.ClassDeclarationContext):
+        self.__nested_in.append(ctx.IDENTIFIER().getText())
+
+    def exitClassDeclaration(self, ctx: JavaParser.ClassDeclarationContext):
+        self.__nested_in.pop(-1)
+
+    def enterMethodBody(self, ctx: JavaParser.MethodBodyContext):
+        self.__in_method_counter += 1
+
+    def exitMethodBody(self, ctx: JavaParser.MethodBodyContext):
+        self.__in_method_counter -= 1
 
     def enterMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        if self.__in_method_counter == 0:
+            self.__last_opened = ctx.start.line
+            self.__current_method = ".".join(self.__nested_in + [ctx.IDENTIFIER().getText()])
         self.count += 1
+
+    def exitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        if self.__in_method_counter == 0:
+            assert self.__last_opened != -1
+            assert self.__current_method is not None
+            last_closed = ctx.methodBody().stop.line
+            self.blocks[self.__current_method] = (self.__last_opened, last_closed)
 
 
 class JavaFile:
     def __init__(self, lines):
+        self.lines = lines
         code = "\n".join(lines)
         codeStream = InputStream(code)
         lexer = JavaLexer(codeStream)
@@ -41,3 +69,11 @@ class JavaFile:
     def count_methods(self):
         listener = self._walk_file(MethodsCountingListener())
         return listener.count
+
+    def eval_blocks(self):
+        listener = self._walk_file(MethodsCountingListener())
+        result = {}
+        for name in listener.blocks:
+            s, f = listener.blocks[name]
+            result[name] = self.lines[s - 1:f]
+        return result

356d9dae1d2a8df5561fc481fac64ef85dc1a462
Ann <annikura@yandex.ru>
2018-05-09 21:03:01 +0300

    Implemented signature retriever

--- java_metrics.py
+++ java_metrics.py
@@ -14,11 +14,11 @@ class ClassCountingListener(JavaParserListener):
 
 
 class MethodsCountingListener(JavaParserListener):
-    def __init__(self):
+    def __init__(self, tokens_stream):
         self.count = 0
+        self.tokens = tokens_stream
         self.blocks = {}
         self.__nested_in = []
-        self.__last_opened = -1
         self.__in_method_counter = 0
         self.__current_method = None
 
@@ -36,16 +36,71 @@ class MethodsCountingListener(JavaParserListener):
 
     def enterMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
         if self.__in_method_counter == 0:
-            self.__last_opened = ctx.start.line
-            self.__current_method = ".".join(self.__nested_in + [ctx.IDENTIFIER().getText()])
+            method_declaration = self.tokens.getText(interval=(ctx.start.tokenIndex, ctx.stop.tokenIndex))
+            method_signature = self.retrieve_signature(method_declaration)
+            method_id = ".".join(self.__nested_in + [method_signature])
+            self.blocks[method_id] = method_declaration.split("\n   ")
         self.count += 1
 
-    def exitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
-        if self.__in_method_counter == 0:
-            assert self.__last_opened != -1
-            assert self.__current_method is not None
-            last_closed = ctx.methodBody().stop.line
-            self.blocks[self.__current_method] = (self.__last_opened, last_closed)
+    @staticmethod
+    def retrieve_signature(first_line: str):
+        closing_bracket_position = first_line.find(')')
+
+        word = ""
+        first_word_sym_position = -1
+        splitted = True
+
+        for i, s in enumerate(first_line):
+            if s == '(':
+                break
+            if s.isalnum():
+                if splitted:
+                    word = ""
+                    first_word_sym_position = i
+                    splitted = False
+                word += s
+                continue
+            splitted = True
+
+        first_line = first_line[first_word_sym_position:closing_bracket_position + 1]
+        template_balance = 0
+        line_without_templates = ""
+
+        for s in first_line:
+            if s == '<':
+                template_balance += 1
+            if template_balance == 0:
+                line_without_templates += s
+            if s == '>':
+                template_balance -= 1
+
+        signature = ""
+        in_type_parameters_list = False
+        right_after_divider = False
+        word_started = False
+
+        for s in line_without_templates:
+            if s in ['(', ')', ',']:
+                if s == '(':
+                    in_type_parameters_list = True
+                if s == ')':
+                    in_type_parameters_list = False
+                right_after_divider = True
+                word_started = False
+                signature += s
+                continue
+            if not s.isalnum():
+                if word_started:
+                    word_started = False
+                    right_after_divider = False
+                continue
+            if not in_type_parameters_list or right_after_divider:
+                word_started = True
+                signature += s
+
+        signature.replace(' ', '')
+        print(signature)
+        return signature
 
 
 class JavaFile:
@@ -54,8 +109,8 @@ class JavaFile:
         code = "\n".join(lines)
         codeStream = InputStream(code)
         lexer = JavaLexer(codeStream)
-        tokensStream = CommonTokenStream(lexer)
-        self.parser = JavaParser(tokensStream)
+        self.tokens_stream = CommonTokenStream(lexer)
+        self.parser = JavaParser(self.tokens_stream)
         self.tree = self.parser.compilationUnit()
 
     def _walk_file(self, listener):
@@ -67,13 +122,9 @@ class JavaFile:
         return listener.count
 
     def count_methods(self):
-        listener = self._walk_file(MethodsCountingListener())
+        listener = self._walk_file(MethodsCountingListener(self.tokens_stream))
         return listener.count
 
     def eval_blocks(self):
-        listener = self._walk_file(MethodsCountingListener())
-        result = {}
-        for name in listener.blocks:
-            s, f = listener.blocks[name]
-            result[name] = self.lines[s - 1:f]
-        return result
+        listener = self._walk_file(MethodsCountingListener(self.tokens_stream))
+        return listener.blocks
